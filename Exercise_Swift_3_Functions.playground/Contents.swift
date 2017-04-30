801cd73cff31e019743f3554a491fd8b149a6a14
Ainurizam <rizainuddin@Ainurizams-MacBook-2.local>
2017-04-30 12:44:45 +0800

    Create New Devslopes Exercise Folder

---
+++ Exercise_Swift_3_Functions.playground/Contents.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+/*: 
+ 
+# Exercise: Swift 3 Functions
+[http://blog.devslopes.com/p/3319a31d-c54e-408a-842d-d37813e39573/](http://blog.devslopes.com/p/3319a31d-c54e-408a-842d-d37813e39573/)
+
+**Requirements:**
+Create a new Xcode Playground
+ 
+*Create a function named add that takes two parameters of type Double and returns the sum of the two numbers*
+*/
+
+
+
+/*:
+*Create a function named subtract that takes two parameters of type Int and returns the difference of the two numbers*
+*/
+
+
+
+/*:
+*Create a function named multiply that takes two parameters of type Float and returns the product of the two numbers*
+*/
+
+
+
+/*:
+*Create a function named divide that takes two parameters of type Double and returns the result of dividing the first parameter by the second*
+*/
+
+
+
+/*:
+**all those functions and pass different values into the parameters - ensure they are producing the correct results**
+*/
