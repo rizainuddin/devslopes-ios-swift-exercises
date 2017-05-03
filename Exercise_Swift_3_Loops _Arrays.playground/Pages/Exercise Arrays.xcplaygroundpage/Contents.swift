e0e9f03e67e91e3229afac9a1b2b91716dbe37ef
Riz <rizainuddin@camelots-mac-mini.local>
2017-05-03 09:40:38 +0800

    Create New Empty Swift 3 Loops & Array Playground File

---
+++ Exercise_Swift_3_Loops _Arrays.playground/Pages/Exercise Arrays.xcplaygroundpage/Contents.swift
@@ -0,0 +1,53 @@
+/*:
+ 
+ # Exercise: Swift 3 Loops & Arrays
+[http://blog.devslopes.com/p/3b84fc52-1d82-4980-bcf8-8c1941f4bfa5/](http://blog.devslopes.com/p/3b84fc52-1d82-4980-bcf8-8c1941f4bfa5/)
+ 
+ **Requirements:**
+ Create a new Xcode Playground
+ 
+ ## Exercise Arrays
+ 
+
+ Declare an empty array of type String
+ */
+ 
+ 
+ 
+/*:
+ Declare an explicit array of type Double and initialize it with 4 values
+ */
+
+
+
+/*:
+ Declare an array and initialize it with 5 values of any type using type inference
+ */
+
+
+
+/*:
+ Use append 3 times in each array
+ */
+
+
+
+/*:
+ Use removeAt in each array
+ */
+
+
+
+/*:
+ Use removeAll on one array
+ */
+
+
+
+/*:
+ Use a new array feature that you haven't learned yet
+ */
+
+ 
+ 
+//: [Next](@next)
---
+++ Exercise_Swift_3_Loops _Arrays.playground/Pages/Exercise Loops.xcplaygroundpage/Contents.swift
@@ -0,0 +1,42 @@
+//: [Previous](@previous)
+
+/*:
+ 
+ # Exercise: Swift 3 Loops & Arrays
+ [http://blog.devslopes.com/p/3b84fc52-1d82-4980-bcf8-8c1941f4bfa5/](http://blog.devslopes.com/p/3b84fc52-1d82-4980-bcf8-8c1941f4bfa5/)
+ 
+ **Requirements:**
+ Create a new Xcode Playground
+ 
+ ## Exercise Loops
+ 
+ 
+ Create an empty array of type Int called oddNumbers
+*/
+
+
+
+/*:
+ Using a standard for-in loop add all odd numbers less than or equal to 100 to the oddNumbers array
+*/
+
+
+
+/*:
+ Create a second array called sums of type Int
+*/
+ 
+
+
+/*:
+ Using a for-in, iterate through oddNumbers array and add the current iteration value + 5 to the sums array
+*/
+
+
+
+/*:
+ Using a repeat while loop, iterate through the sums array and print "The sum is: x" where x is the current value of the iteration (ie The sum is: 15)
+*/
+
+
+
