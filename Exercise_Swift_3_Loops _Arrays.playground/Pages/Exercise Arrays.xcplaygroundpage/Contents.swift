f24b8fd8aac7dd2d7b312142ec9be09162ecf9e8
Ainurizam Ainuddin <rizainuddin@Ainurizams-MacBook-2.local>
2017-05-05 08:18:50 +0800

    Complete Array Exercise

--- Exercise_Swift_3_Loops _Arrays.playground/Pages/Exercise Arrays.xcplaygroundpage/Contents.swift
+++ Exercise_Swift_3_Loops _Arrays.playground/Pages/Exercise Arrays.xcplaygroundpage/Contents.swift
@@ -11,43 +11,48 @@
 
  Declare an empty array of type String
  */
- 
- 
- 
+var medicineForWind = [String]()
 /*:
  Declare an explicit array of type Double and initialize it with 4 values
  */
-
-
-
+var foreignExchange: [Double] = [4.34, 1000.12, 2.33, 3.66]
 /*:
  Declare an array and initialize it with 5 values of any type using type inference
  */
-
-
-
+var countryCurrency = ["Ringgit", "Rupiah", "Dinar", "Dollar", "Baht"]
 /*:
  Use append 3 times in each array
  */
-
-
-
+countryCurrency.append("Pound Sterling")
+countryCurrency.append("Yen")
+countryCurrency.append("Forint")
 /*:
  Use removeAt in each array
  */
+countryCurrency.remove(at: 0)
+countryCurrency.remove(at: 1)
+countryCurrency.remove(at: 2)
+countryCurrency.remove(at: 3)
+countryCurrency
+countryCurrency.remove(at: 0)
+countryCurrency.remove(at: 1)
+countryCurrency
+countryCurrency.remove(at: 0)
+countryCurrency.remove(at: 0)
 
-
-
+// Must be a better way to remove in one line of code. I have to keep checking the array everytime when it is removed. I can just remove(at: 0) at every line. Sheesh.
 /*:
  Use removeAll on one array
  */
-
-
-
+countryCurrency += ["Ringgit", "Rupiah", "Dinar", "Dollar", "Baht", "Pound Sterling", "Yen", "Forint"]
+countryCurrency.removeAll()
 /*:
  Use a new array feature that you haven't learned yet
  */
+countryCurrency += ["Ringgit", "Rupiah", "Dinar", "Dollar", "Baht", "Pound Sterling", "Yen", "Forint"]
+countryCurrency.count
+countryCurrency.customMirror
+countryCurrency.debugDescription
+countryCurrency.isEmpty
 
- 
- 
 //: [Next](@next)
